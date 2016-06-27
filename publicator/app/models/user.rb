15674da5ffbd0e7c63b0da7b2e102bdfd32fda72
diego <diego@email>
2016-06-26 21:55:25 -0300

    Add user session

--- publicator/app/models/user.rb
+++ publicator/app/models/user.rb
@@ -1,9 +1,16 @@
 class User < ActiveRecord::Base
   EMAIL_REGEXP = /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
 
+  scope :confirmed, -> { where.not(confirmed_at: nil) }
+
   validates_presence_of :email, :name
   validates_uniqueness_of :email
 
   validates_format_of :email, with: EMAIL_REGEXP
   has_secure_password
+
+  def self.authenticate(email, password)
+      find_by(email: email).
+      try(:authenticate, password)
+  end
 end
--- publicator/config/routes.rb
+++ publicator/config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   resources :pulications
   resources :users
 
+  resource :confirmation, only: [:show]
+  resource :user_sessions, only: [:create, :new, :destroy]
+
+
   root 'home#index'  
 end
