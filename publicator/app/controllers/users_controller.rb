d1edfd5acf84dbeb74712d3b89bf7057e7b02b58
diego <diego@email>
2016-06-26 23:24:36 -0300

    Add authentication filters

--- publicator/app/controllers/users_controller.rb
+++ publicator/app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :require_no_authentication, only: [:new, :create]
+  before_action :can_change, only: [:edit, :update]
+
   def new
     @user = User.new
   end
@@ -40,5 +43,15 @@ class UsersController < ApplicationController
     require(:user).
     permit(:email, :name, :lates, :password,
            :password_confirmation)
+  end
+
+  def can_change
+    unless user_signed_in? && current_user == user
+      redirect_to user_path(params[:id])
+    end
+  end
+
+  def user
+    @user ||= User.find(params[:id])
   end
 end
