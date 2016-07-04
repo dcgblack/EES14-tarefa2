268e4f0a8734fa530990b7de69bf86fadafb4972
diego.garcia <diego.garcia@datacom.ind.br>
2016-07-04 16:33:52 -0300

    Populate the database with publications

--- publicator/db/seeds.rb
+++ publicator/db/seeds.rb
@@ -1,7 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.delete_all
+
+params =  { :user =>
+    {
+    :name => 'kratos',
+    :email => 'kratos@sparta.com',
+    :password => '123',
+    :password_confirmation => '123'
+  }
+}
+
+current_user = User.create!(params[:user])
+
+params =  { :publication =>
+    {
+    :category => 'Artigos',
+    :title => 'Ciclo de vida dos Preas',
+    :subtitle => 'Prea eh mato',
+    :edition => '2º',
+    :local => 'Eldorado do Sul',
+    :publishing_company => 'Revista Roedores',
+    :year_of_publication => '2012',
+    :initial_final_page => '7-22',
+    :volume => '1',
+    :publication_number => '3',
+    :authors_attributes => [{
+        :name => 'Argel',
+        :surname => 'Fulks',
+        :curriculum_lattes => 'http://www.google.com',
+        :institution => 'Escola das Ruas do RS',
+      }]
+  }
+}
+
+current_user.publications.build(params[:publication]).save
